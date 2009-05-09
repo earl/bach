469f968d4f9853bac673d492cf45aa97f5b238d3
Andreas Bolka <a@bolka.at>
2009-05-09 21:04:44 +0200

    Whipped into structure.
    - Directly submit to the SSOLogin servlet
    - logout() only succeeds if it results in HTTP 503 Unauthorized
    - Account selection refactored
    - Custom error hierarchy added; currently only consisting of an
      `AccountNotFoundError`
    - Navigation to account detail pages always starts from the overview
      page to enable navigation to multiple accounts in one BachBrowser
      session
    - Made all helper functions internal, added `read_account` facade
    - Proper main entry point that tries to always logout if any error is
      encountered

--- bach.py
+++ bach.py
@@ -1,58 +1,100 @@
 #!/usr/bin/env python
 from getpass import getpass
-from mechanize import Browser, FormNotFoundError, HTTPError
+from mechanize import Browser, FormNotFoundError, HTTPError, LinkNotFoundError
+from urllib import urlencode
 
-# config
 config = {
     'u': str(input('User Code: ')),
     'p': getpass('PIN: '),
     'n': str(input('Account Number: '))
 }
 
-# init
-b = Browser()
+def servlet_url(name):
+    return 'https://online.bankaustria.at/servlet/%s' % name
 
-# login
-b.open('https://online.bankaustria.at/bach/de/login/login.html')
-b.select_form('Frame_Login')
-b.form.action = 'https://online.bankaustria.at/servlet/SSOLogin'
-b['yzbks'] = config['u']
-b['jklwd'] = config['p']
-b.submit()
+def is_account_link(link):
+    onclick = dict(link.attrs).get('onclick')
+    return onclick and onclick.startswith('javascript:submit_detail(')
 
-# open the content frame
-b.follow_link(name='content')
+class BachError(Exception): pass
+class AccountNotFoundError(BachError): pass
 
-# ignore the "please don't forget to logout" page
-try:
-    b.select_form('Main')
-    b.submit()
-except FormNotFoundError:
-    pass
+class BachBrowser(object):
+    def __init__(self):
+        self._b = Browser()
 
-# navigate to detail page
-b.select_form('Main')
-link = b.find_link(text_regex=r'%s' % config['n'])
-servlet = 'https://online.bankaustria.at/servlet/GiroKontoDetail'
-sessionid = b['sessionid']
-paramstr = dict(link.attrs)['onclick'].split(',')[1].strip("'")
-b.open('%s?sessionid=%s&language=DE&mode=no%s' % (
-        servlet, sessionid, paramstr))
+    def login(self, user, pin):
+        # submit login form
+        login_data = urlencode({'yzbks': user, 'jklwd': pin})
+        self._b.open(servlet_url('SSOLogin'), login_data)
 
-# download csv
-b.select_form('PrintDownload')
-b.form.action = 'https://online.bankaustria.at/servlet/GiroKontoDetail'
-b.form.set_all_readonly(False)
-b['downloadfilename'] = 'records.csv'
-b['downloadmode'] = 'csvyes'
-b['printmode'] = 'no'
-r = b.submit()
+        # open the content frame
+        self._b.follow_link(name='content')
 
-# print csv
-print r.read(),
+        # ack the "please don't forget to logout" form (if present)
+        try:
+            self._b.select_form('Main')
+            self._b.submit()
+        except FormNotFoundError:
+            pass
 
-# logout
-try:
-    b.open('https://online.bankaustria.at/servlet/Logout')
-except HTTPError:
-    pass
+        # store current session id
+        self._b.select_form('Main')
+        self._session_id = self._b['sessionid']
+
+        # store the url of the "overview" page
+        self._root_url = self._b.geturl()
+
+    def logout(self):
+        try:
+            self._b.open(servlet_url('Logout'))
+        except HTTPError, e:
+            if e.code != 503:
+                raise e
+
+    def _read_response(self):
+        return self._b.response().read()
+
+    def _open_root(self):
+        self._b.open(self._root_url)
+
+    def _find_account(self, account_re):
+        # assumes that the browser is on the proper page (root)
+        try:
+            link = self._b.find_link(text_regex=account_re,
+                                     predicate=is_account_link)
+        except LinkNotFoundError, e:
+            raise AccountNotFoundError(account_re)
+        args = dict(link.attrs)['onclick'].split("'")
+        return args[1], args[3]
+
+    def _open_account(self, account_re):
+        # assumes that the browser is on the proper page (root)
+        servlet, params = self._find_account(account_re)
+        self._b.open('%s?sessionid=%s&language=DE&mode=no%s' % (
+                servlet_url(servlet), self._session_id, params))
+
+    def _open_download(self, mode='csv'):
+        # assumes that the browser is on the proper page (account detail)
+        self._b.select_form('PrintDownload')
+        self._b.form.action = servlet_url('GiroKontoDetail')
+        self._b.form.set_all_readonly(False)
+        self._b['downloadfilename'] = 'records.%s' % mode
+        self._b['downloadmode'] = '%syes' % mode
+        self._b['printmode'] = 'no'
+        self._b.submit()
+
+    def read_account(self, account, mode='csv'):
+        self._open_root()
+        self._open_account(account)
+        self._open_download(mode)
+        return self._read_response()
+
+
+if __name__ == '__main__':
+    b = BachBrowser()
+    b.login(config['u'], config['p'])
+    try:
+        print b.read_account(config['n']),
+    finally:
+        b.logout()
