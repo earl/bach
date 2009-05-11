895159037d53a33dc861458491b332016ad1276e
Andreas Bolka <a@bolka.at>
2009-05-11 03:46:59 +0200

    Release boilerplate.

--- bach.py
+++ bach.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python
+"""A scraper for Bank Austria's online banking"""
+
+__license__ = '''
+Copyright (C) 2009 Andreas Bolka
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
+
+__version__ = '0.0.0'
+
 from mechanize import Browser, FormNotFoundError, HTTPError, LinkNotFoundError
 from urllib import urlencode
-
-
 
 class BachError(Exception): pass
 class AccountNotFoundError(BachError): pass
